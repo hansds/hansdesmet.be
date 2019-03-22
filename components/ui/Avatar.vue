2027121e3dbc476b39887bf77f6e8f1f31c8146f
Hans De Smet <hello@hansdesmet.be>
2019-03-22 14:25:24 +0100

    Fix for masking

--- components/ui/Avatar.vue
+++ components/ui/Avatar.vue
@@ -1,10 +1,13 @@
 <template>
-  <div class="avatar">
+  <div :class="styleClasses">
     <v-lazy-image
       srcset="avatar@1x.jpg 400w, avatar@2x.jpg 800w"
       sizes="(max-width: 320px) 280px, 440px"
       src="/avatar@2x.jpg"
-      class="avatar__image"/>
+      class="avatar__image"
+      alt="Hans De Smet"
+      @load="onLoad"
+      />
   </div>
 </template>
 
@@ -12,18 +15,39 @@
 import VLazyImage from "v-lazy-image";
 
 export default {
-components: {
-  VLazyImage,
-},
+  components: {
+    VLazyImage,
+  },
+  data() {
+    return {
+      isLoaded: false
+    }
+  },
+  computed: {
+    styleClasses() {
+      return {
+        'avatar': true,
+        'avatar--loaded': this.isLoaded,
+      }
+    },
+  },
+  methods: {
+    onLoad() {
+      this.isLoaded = true;
+    },
+  },
 }
 </script>
 
 <style lang="scss" scoped>
 .avatar {
   border-radius: 50%;
-  overflow: hidden;
   display: inline-block;
-  -webkit-mask-image: -webkit-radial-gradient(white, black);
+
+  &--loaded {
+    overflow: hidden;
+    -webkit-mask-image: -webkit-radial-gradient(white, black);
+  }
 
   &__image {
     max-width: 100%;
